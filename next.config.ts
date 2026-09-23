11c30152d2a7f73496e080ffc3e3fcb62b047719
Nayanchandrakar <nayanswe@gmail.com>
2026-09-23 21:28:59 +0530

    Update next.config.ts to include OpenTelemetry packages for server external dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@opentelemetry/auto-instrumentations-node",
+    "@opentelemetry/instrumentation-winston",
+  ],
 }
 
 export default nextConfig
